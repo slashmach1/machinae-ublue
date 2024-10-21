f9748f5142641f1750fa61fd6ef2569fb31ac02b
slashmach1 <slashmach1@gmail.com>
2024-10-21 11:15:17 -0500

    hp-wmi update 2

---
+++ files/scripts/build-kmod-hp-wmi.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+set -oeux pipefail
+
+ARCH="$(rpm -E '%_arch')"
+KERNEL="$(rpm -q "${KERNEL_NAME}" --queryformat '%{VERSION}-%{RELEASE}.%{ARCH}')"
+RELEASE="$(rpm -E '%fedora')"
+cp /tmp/ublue-os-akmods-addons/rpmbuild/SOURCES/_copr_ublue-os-akmods.repo /etc/yum.repos.d/
+dnf install -y \
+     /usr/src/hp-wmi/*.rpm
+akmods --force --kernels "${KERNEL}" --kmod hp-wmi
+modinfo /usr/lib/modules/${KERNEL}/extra/hp-wmi/hp-wmi.ko.xz  > /dev/null \
+|| (find /var/cache/akmods/hp-wmi/ -name \*.log -print -exec cat {} \; && exit 1)
+rm -f /etc/yum.repos.d/_copr_ublue-os-akmods.repo
